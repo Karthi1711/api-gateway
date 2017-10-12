c78f36bb27f7b2706f6700b10043fb8b1002390b
Chandrasekaran <ashokkumar.chandrasekaran@merrillcorp.com>
2017-10-12 14:40:19 +0530

    OMS-0019 yml change

--- src/main/java/com/oms/apigateway/controller/DocumentationController.java
+++ src/main/java/com/oms/apigateway/controller/DocumentationController.java
@@ -2,7 +2,13 @@ package com.oms.apigateway.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
@@ -16,12 +22,12 @@ public class DocumentationController implements SwaggerResourcesProvider {
     @Override
     public List<SwaggerResource> get() {
         List resources = new ArrayList();
-        resources.add(swaggerResource("customer-service", "/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("inventory-service", "/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("product-service", "/product/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("event-service", "/event/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("mail-service", "/mail/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("order-comp-service", "/order/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("customer-service", "/api/customer/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("inventory-service", "/api/inventory/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("product-service", "/api/product/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("event-service", "/api/event/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("mail-service", "/api/mail/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("order-comp-service", "/api/order/v2/api-docs", "2.0"));
         return resources;
     }
     private SwaggerResource swaggerResource(String name, String location, String version) {
@@ -30,6 +36,13 @@ public class DocumentationController implements SwaggerResourcesProvider {
         swaggerResource.setLocation(location);
         swaggerResource.setSwaggerVersion(version);
         return swaggerResource;
+    }
+
+    @ResponseBody
+    @RequestMapping("/swagger-resources/configuration/security")
+    public ResponseEntity<SecurityConfiguration> securityConfiguration() {
+        return new ResponseEntity<>(new SecurityConfiguration(null, null, null, null, null, ApiKeyVehicle.HEADER,
+                "api_key", ","), HttpStatus.OK);
     }
 
 }
